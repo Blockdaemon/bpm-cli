6e03a71cf515c8d2ca1b32bb42bfdbe0e88eb30f
Jonas Pfannschmidt <jonas@blockdaemon.com>
2019-06-26 16:18:45 +0100

    Cleanup and document docker library

--- pkg/docker/docker.go
+++ pkg/docker/docker.go
@@ -1,10 +1,25 @@
+// A simple docker abstraction layer.
+//
+// Please note that the methods are idempotent (i.e. they can be called multiple times without changing the result).
+// This is important because it reduces the need for additional checks if the user runs a command multiple times. E.g.
+// the code that uses this package doesn't need to check if the container already runs, ContainerRuns does that internally
+// and just does nothing if the container is already running.
+//
+// Additionally it sometimes makes error handling simpler. If an particular method failed halfway, it can just be called 
+// again without causing any issues.
+//
+// The general pattern used internally in this package is:
+//
+// 1. Check if the desired result (e.g. container running) already exists
+// 2. If yes, do nothing
+// 3. If no, invoke the action that produces the result (e.g. run container)
 package docker
 
 import (
 	"context"
 	"fmt"
 	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
+	dockercontainer "github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/mount"
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
@@ -14,14 +29,11 @@ import (
 	"bufio"
 )
 
-// ----------------------------------------------------------------------------
-// BasicManager
-// ----------------------------------------------------------------------------
-
 type BasicManager struct {
 	cli *client.Client
 }
 
+// NewBasicManager creates a BasicManager
 func NewBasicManager() (*BasicManager, error) {
 	cli, err := client.NewEnvClient()
 	if err != nil {
@@ -33,7 +45,7 @@ func NewBasicManager() (*BasicManager, error) {
 	}, nil
 }
 
-// RemoveContainer stops and removes a container
+// ContainerAbset stops and removes a container if it is running/exists
 func (bm *BasicManager) ContainerAbsent(ctx context.Context, containerName string) error {
 	running, err := bm.isContainerRunning(ctx, containerName)
 	if err != nil {
@@ -48,7 +60,7 @@ func (bm *BasicManager) ContainerAbsent(ctx context.Context, containerName strin
 		}
 	}
 
-	exists, err := bm.doesContainerExists(ctx, containerName)
+	exists, err := bm.doesContainerExist(ctx, containerName)
 	if err != nil {
 		return err
 	}
@@ -64,10 +76,15 @@ func (bm *BasicManager) ContainerAbsent(ctx context.Context, containerName strin
 	return nil
 }
 
+// NetworkExists creates a network if it doesn't exist yet
 func (bm *BasicManager) NetworkExists(ctx context.Context, networkID string) error {
-	_, err := bm.cli.NetworkInspect(ctx, networkID)
+	exists, err := bm.doesNetworkExist(ctx, networkID)
+	if err != nil {
+		return err
+	}
 
-	if client.IsErrNetworkNotFound(err) { 
+	if !exists {
+		fmt.Printf("Creating network '%s'\n", networkID)
 		_, err := bm.cli.NetworkCreate(ctx, networkID, types.NetworkCreate{ CheckDuplicate: true, })
 
 		if err != nil {
@@ -75,34 +92,68 @@ func (bm *BasicManager) NetworkExists(ctx context.Context, networkID string) err
 		}
 	}
 
-	return err
+	return nil
 }
 
-type ContainerMountParameter struct {
+// Mount defines a docker volume mount
+type Mount struct {
 	From string;
 	To string;
 }
 
-type ContainerPortParameter struct {
+// Port defines a forwarded docker port
+type Port struct {
 	HostIP string;
 	HostPort string;
 	ContainerPort string;
 	Protocol string;
 }
 
-
-
-type ContainerRunParameters struct {
-	ContainerName string;
-	ContainerImage string;
-	NetworkName string;
+// Container defines all parameters used to create a container
+type Container struct {
+	Name string;
+	Image string;
+	NetworkID string;
 	EnvFilename string;
-	Mounts []ContainerMountParameter;
-	Ports []ContainerPortParameter;
-
+	Mounts []Mount;
+	Ports []Port;
 }
 
-func (bm *BasicManager) doesContainerExists(ctx context.Context, containerName string) (bool, error) {
+// ContainerRuns creates and starts a container if it doesn't exist/run yet
+func (bm *BasicManager) ContainerRuns(ctx context.Context, container Container) error {
+	if err := bm.pullImage(ctx, container.Image); err != nil {
+		return err
+	}
+
+	exists, err := bm.doesContainerExist(ctx, container.Name)
+	if err != nil {
+		return err
+	}
+
+	if !exists {
+		fmt.Printf("Creating container '%s'\n", container.Name)	
+
+		if err := bm.createContainer(ctx, container); err != nil {
+			return err
+		}
+	}
+
+	running, err := bm.isContainerRunning(ctx, container.Name)
+	if err != nil {
+		return err
+	}
+	if !running {
+		fmt.Printf("Starting container '%s'\n", container.Name)
+
+		if err := bm.cli.ContainerStart(ctx, container.Name, types.ContainerStartOptions{}); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (bm *BasicManager) doesContainerExist(ctx context.Context, containerName string) (bool, error) {
 	_, err := bm.cli.ContainerInspect(ctx, containerName)
 	if err != nil {
 		if client.IsErrContainerNotFound(err) { 
@@ -113,6 +164,21 @@ func (bm *BasicManager) doesContainerExists(ctx context.Context, containerName s
 	}
 
 	return true, nil
+}
+
+func (bm *BasicManager) doesNetworkExist(ctx context.Context, networkID string) (bool, error) {
+	_, err := bm.cli.NetworkInspect(ctx, networkID)
+	if err != nil {
+		if client.IsErrNetworkNotFound(err) {
+			return false, nil
+		}
+
+		return false, err
+	}
+
+
+	return true, nil
+
 }
 
 func (bm *BasicManager) isContainerRunning(ctx context.Context, containerName string) (bool, error) {
@@ -129,116 +195,92 @@ func (bm *BasicManager) isContainerRunning(ctx context.Context, containerName st
 
 }
 
-// ContainerRuns starts a container if it doesn't run yet
-func (bm *BasicManager) ContainerRuns(ctx context.Context, params ContainerRunParameters) error {
-	// Pull an image from the repository
-	out, err := bm.cli.ImagePull(ctx, params.ContainerImage, types.ImagePullOptions{})
+func (bm *BasicManager) pullImage(ctx context.Context, imageName string) error {
+	out, err := bm.cli.ImagePull(ctx, imageName, types.ImagePullOptions{})
 	if err != nil {
 		return err
 	}
 	defer out.Close()
     if _, err := ioutil.ReadAll(out); err != nil {
-            panic(err)
+        return err
     }
 
-	exists, err := bm.doesContainerExists(ctx, params.ContainerName)
-	if err != nil {
-		return err
-	}
-	if exists {
-		fmt.Printf("Container '%s' already exists. Skipping creation.\n", params.ContainerName)
-	} else {
-		fmt.Printf("Creating container '%s'\n", params.ContainerName)	
+    return nil
+}
 
-		// Environment variable configuration
-		var envs []string
-		if params.EnvFilename != "" {
-			envs, err = readLines(params.EnvFilename)
-			if err != nil {
-				return err
-			}
-		}
-
-		// Container's config
-		containerCfg := &container.Config{
-			Image: params.ContainerImage,
-			Env: envs,
-		}
-
-		// Host's config
-
-		portBindings := make(map[nat.Port][]nat.PortBinding)
-
-
-		for _, portParameter := range params.Ports {
-			containerPort, err := nat.NewPort(portParameter.Protocol, portParameter.ContainerPort)
-			if err != nil {
-				return err
-			}
-
-			portBindings[containerPort] = []nat.PortBinding{
-				nat.PortBinding{
-					HostIP: portParameter.HostIP,
-					HostPort: portParameter.HostPort,
-				},
-			}
-
-		}
-
-
-		hostCfg := &container.HostConfig{
-			PortBindings: portBindings,
-
-			RestartPolicy: container.RestartPolicy{
-				Name: "unless-stopped",
-			},
-
-			LogConfig: container.LogConfig{
-				Type: "json-file",
-				Config: map[string]string{
-					"max-size": "10m",
-					"max-file": "3",
-				},
-			},
-		}
-
-		// Mountpoints
-		for _, mountParam := range params.Mounts {
-			hostCfg.Mounts = append(hostCfg.Mounts, mount.Mount {
-                Type:   mount.TypeBind,
-                Source: mountParam.From,
-                Target: mountParam.To,
-			})
-		}
-
-		// Network configuration
-		endpointsConfig := make(map[string]*network.EndpointSettings)
-		endpointsConfig[params.NetworkName] = &network.EndpointSettings{
-			NetworkID: params.NetworkName,
-		}
-		networkConfig := &network.NetworkingConfig{
-			EndpointsConfig: endpointsConfig,
-		}
-
-		// Create a container with configs
-		_, err := bm.cli.ContainerCreate(ctx, containerCfg, hostCfg, networkConfig, params.ContainerName)
+func (bm *BasicManager) createContainer(ctx context.Context, container Container) error {
+	// Environment variables
+	var envs []string
+	var err error
+	if container.EnvFilename != "" {
+		envs, err = readLines(container.EnvFilename)
 		if err != nil {
 			return err
 		}
 	}
 
-	running, err := bm.isContainerRunning(ctx, params.ContainerName) 
-	if err != nil {
-		return err
-	}
-	if running {
-		fmt.Printf("Container '%s' is already running. Skipping start.\n", params.ContainerName)
-	} else {
-		fmt.Printf("Starting container '%s'\n", params.ContainerName)	
+	// Ports
+	portBindings := make(map[nat.Port][]nat.PortBinding)
 
-		if err := bm.cli.ContainerStart(ctx, params.ContainerName, types.ContainerStartOptions{}); err != nil {
+	for _, portParameter := range container.Ports {
+		containerPort, err := nat.NewPort(portParameter.Protocol, portParameter.ContainerPort)
+		if err != nil {
 			return err
 		}
+
+		portBindings[containerPort] = []nat.PortBinding{
+			nat.PortBinding{
+				HostIP: portParameter.HostIP,
+				HostPort: portParameter.HostPort,
+			},
+		}
+	}
+
+	// Mountpoints
+	var mounts []mount.Mount 
+	for _, mountParam := range container.Mounts {
+		mounts = append(mounts, mount.Mount {
+            Type:   mount.TypeBind,
+            Source: mountParam.From,
+            Target: mountParam.To,
+		})
+	}
+
+	// Host config
+	hostCfg := &dockercontainer.HostConfig{
+		Mounts: mounts,
+		PortBindings: portBindings,
+		RestartPolicy: dockercontainer.RestartPolicy{
+			Name: "unless-stopped",
+		},
+		LogConfig: dockercontainer.LogConfig{
+			Type: "json-file",
+			Config: map[string]string{
+				"max-size": "10m",
+				"max-file": "3",
+			},
+		},
+	}
+
+	// Network config
+	endpointsConfig := make(map[string]*network.EndpointSettings)
+	endpointsConfig[container.NetworkID] = &network.EndpointSettings{
+		NetworkID: container.NetworkID,
+	}
+	networkConfig := &network.NetworkingConfig{
+		EndpointsConfig: endpointsConfig,
+	}
+
+	// Container config
+	containerCfg := &dockercontainer.Config{
+		Image: container.Image,
+		Env: envs,
+	}
+
+	// Create a container with configs
+	_, err = bm.cli.ContainerCreate(ctx, containerCfg, hostCfg, networkConfig, container.Name)
+	if err != nil {
+		return err
 	}
 
 	return nil
@@ -258,4 +300,3 @@ func readLines(path string) ([]string, error) {
     }
     return lines, scanner.Err()
 }
-
