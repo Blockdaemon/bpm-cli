49e4bad199b4b042fe8593617dce235a1d87c29e
Jonas Pfannschmidt <jonas@blockdaemon.com>
2019-11-18 15:59:02 +0000

    Simple smoke test

---
+++ smoke-test.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# This is a simple test that runs through all bpm commands. 
+
+basedir=./build/bpm
+
+function getStatusColumn() {
+	echo $(go run cmd/main.go --base-dir $basedir status | cut -d'|' -f$1 | tail -n2 | head -n1 | tr -d " ")
+}
+
+function checkStatus() {
+	status=$(getStatusColumn 2) 
+
+	if [[ $status != $1 ]];then
+		exit 1
+	fi
+}
+
+function cleanup {
+	echo ">>> CLEANING UP - IGNORE ANY ERRORS AFTER THIS"
+	set +e # Run cleanup regardless of errors
+	set +x # Make output a bit nicer
+
+	# Stop node if it is still running
+	go run cmd/main.go --base-dir $basedir stop $(getStatusColumn 1) &> /dev/null
+}
+
+function setup {
+	# Clean up from previous test runs
+	if [[ -d $basedir ]]; then
+		/bin/rm -r $basedir
+	fi
+
+	trap cleanup EXIT
+
+	set -e # Stop on first error
+	set -x # Print commands
+
+}
+
+setup
+
+go run cmd/main.go --base-dir $basedir version
+go run cmd/main.go --base-dir $basedir list
+go run cmd/main.go --base-dir $basedir search polkadot
+go run cmd/main.go --base-dir $basedir install polkadot
+go run cmd/main.go --base-dir $basedir info polkadot
+go run cmd/main.go --base-dir $basedir configure polkadot
+go run cmd/main.go --base-dir $basedir status
+checkStatus "stopped"
+nodeID=$(getStatusColumn 1) 
+go run cmd/main.go --base-dir $basedir status
+go run cmd/main.go --base-dir $basedir start $nodeID
+checkStatus "running"
+go run cmd/main.go --base-dir $basedir show node $nodeID
+go run cmd/main.go --base-dir $basedir show config $nodeID
+go run cmd/main.go --base-dir $basedir test $nodeID
+go run cmd/main.go --base-dir $basedir stop $nodeID
+go run cmd/main.go --base-dir $basedir remove --config $nodeID
+go run cmd/main.go --base-dir $basedir remove --data $nodeID
+go run cmd/main.go --base-dir $basedir remove --all $nodeID
+go run cmd/main.go --base-dir $basedir uninstall polkadot
+
+echo ">>> DONE, ALL TESTS RAN SUCCESSFUL"
