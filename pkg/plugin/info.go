a168fd369d0f3d74b1f170f858de0ac3005123c5
Jonas Pfannschmidt <jonas@blockdaemon.com>
2019-11-19 17:28:56 +0000

    Make `info` look nicer and make it work if plugin is not installed

--- pkg/plugin/info.go
+++ pkg/plugin/info.go
@@ -2,7 +2,6 @@ package plugin
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/Blockdaemon/bpm/pkg/pbr"
 )
@@ -15,23 +14,59 @@ func (p *PluginCmdContext) Info(pluginName string) error {
 		return err
 	}
 
-	parameterOptions, err := p.getParameterOptions(pluginName)
-	if err != nil {
-		return err
-	}
-
 	fmt.Printf("Name:         %s\n", versions[0].Package.Name)
 	fmt.Printf("Description:  %s\n", versions[0].Package.Description)
-	fmt.Printf("Protocol:     %s\n", strings.Join(parameterOptions.Protocol, ", "))
-	fmt.Printf("Network:      %s\n", strings.Join(parameterOptions.Network, ", "))
-	fmt.Printf("Network Type: %s\n", strings.Join(parameterOptions.NetworkType, ", "))
-	fmt.Printf("Subtype:      %s\n", strings.Join(parameterOptions.Subtype, ", "))
-	prefix := "Versions:     "
+
+	if p.isInstalled(pluginName) {
+		parameterOptions, err := p.getParameterOptions(pluginName)
+		if err != nil {
+			return err
+		}
+
+		for ix, protocol := range parameterOptions.Protocol {
+			if ix == 0 {
+				fmt.Printf("Protocol:     %s (default)\n", protocol)
+			} else {
+				fmt.Printf("              %s\n", protocol)
+			}
+		}
+
+		for ix, network := range parameterOptions.Network {
+			if ix == 0 {
+				fmt.Printf("Network:      %s (default)\n", network)
+			} else {
+				fmt.Printf("              %s\n", network)
+			}
+		}
+
+		for ix, networkType := range parameterOptions.NetworkType {
+			if ix == 0 {
+				fmt.Printf("Network Type: %s (default)\n", networkType)
+			} else {
+				fmt.Printf("              %s\n", networkType)
+			}
+		}
+
+		for ix, subType := range parameterOptions.Subtype {
+			if ix == 0 {
+				fmt.Printf("Subtype:      %s (default)\n", subType)
+			} else {
+				fmt.Printf("              %s\n", subType)
+			}
+		}
+	}
+
+	installedVersion := p.getInstalledVersion(pluginName)
 	for ix, version := range versions {
-		fmt.Printf("%s%s\n", prefix, version.Version)
+		installedPlaceholder := ""
+		if version.Version == installedVersion {
+			installedPlaceholder = " (installed)"
+		}
 
 		if ix == 0 {
-			prefix = "              "
+			fmt.Printf("Versions:     %s%s\n", version.Version, installedPlaceholder)
+		} else {
+			fmt.Printf("              %s\n", version.Version)
 		}
 	}
 
